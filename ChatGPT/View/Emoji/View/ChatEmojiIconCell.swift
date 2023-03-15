32e30f91938a37772374a7cfe6b80368088b7720
YeQiu <793983383@qq.com>
2023-03-15 18:14:54 +0800

    fix

---
+++ ChatGPT/View/Emoji/View/ChatEmojiIconCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatEmojiIconCell.swift
+//  ChatGPT
+//
+//  Created by Ray on 2023/3/15.
+//
+
+import UIKit
+
+class ChatEmojiIconCell: UICollectionViewCell {
+
+    @IBOutlet weak var textLab: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- ChatGPT/View/Emoji/View/ChatEmojiItemCell.swift
+++ ChatGPT/View/Emoji/View/ChatEmojiItemCell.swift
@@ -1,5 +1,5 @@
 //
-//  ChatEmojiContentView.swift
+//  ChatEmojiItemCell.swift
 //  ChatGPT
 //
 //  Created by Ray on 2023/3/15.
@@ -7,77 +7,72 @@
 
 import UIKit
 
-class ChatEmojiContentView: UIView {
+class ChatEmojiItemCell: UICollectionViewCell {
     lazy var flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
     lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.flowLayout)
-    var cellDict: [String: String] = [:]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.initCommon()
+        initCommon()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.initCommon()
+        initCommon()
     }
-    
     func initCommon() {
         // 设置collectionView属性
-        
+        self.backgroundColor = .red
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.backgroundColor = .white
-        collectionView.isPagingEnabled = true
+        collectionView.isPagingEnabled = false
+        collectionView.register(UINib.init(nibName: "ChatEmojiIconCell", bundle: nil), forCellWithReuseIdentifier: "ChatEmojiIconCell")
         
-        flowLayout.scrollDirection = .horizontal
+        flowLayout.scrollDirection = .vertical
         flowLayout.minimumLineSpacing = 0
         flowLayout.minimumInteritemSpacing = 0
-        flowLayout.sectionInset = .zero
+        flowLayout.sectionInset = .init(top: 0, left: 0, bottom: self.safeAreaInsets.bottom, right: 0)
         
         // 将collectionView添加到视图中
         self.addSubview(collectionView)
         collectionView.contentInsetAdjustmentBehavior = .never
-        
         collectionView.snp.makeConstraints { make in
-            make.left.right.top.equalTo(0)
-            make.bottom.equalTo(0)
+            make.left.right.top.bottom.equalTo(0)
         }
-        
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        collectionView.reloadData()
+        flowLayout.sectionInset = .init(top: 0, left: 0, bottom: self.safeAreaInsets.bottom, right: 0)
+        self.collectionView.reloadData()
     }
     
-    func registCell(identifier: String) {
-        if self.cellDict[identifier] == nil {
-            collectionView.register(ChatEmojiItemCell.self, forCellWithReuseIdentifier: identifier)
-            self.cellDict[identifier] = identifier
-        }
-        
-    }
 }
 
-extension ChatEmojiContentView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
+
+extension ChatEmojiItemCell: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let identifier = "ChatEmojiItemCell_\(indexPath.row)"
-        self.registCell(identifier: identifier)
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! ChatEmojiItemCell
         
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatEmojiIconCell", for: indexPath) as! ChatEmojiIconCell
+        cell.textLab.text = "\(indexPath.row)"
+        let red = CGFloat(arc4random()%255) / 255.0
+        let green = CGFloat(arc4random()%255) / 255.0
+        let blue = CGFloat(arc4random()%255) / 255.0
+        cell.backgroundColor = UIColor.init(red: red, green: green, blue: blue, alpha: 1)
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return self.bounds.size
+        let width = self.bounds.size.width / 4
+        return CGSize.init(width: width, height: width)
     }
 }
